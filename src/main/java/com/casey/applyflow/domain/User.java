a810af423b2d577c15a1d7c6332ebb817f228f89
CaseyDevs <CaseyDevs5@gmail.com>
2026-01-27 20:32:38 +0000

    feat: add jpa persistence to user domain

--- src/main/java/com/casey/applyflow/domain/User.java
+++ src/main/java/com/casey/applyflow/domain/User.java
@@ -3,11 +3,36 @@ package com.casey.applyflow.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "`user`")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false, unique = true)
     private String email;
-    private String password;
-    private List<Application> applications = new ArrayList<>(); // Temp before persistence
+
+    @Column(nullable = false)
+    private String password;  // must be hashed later
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Application> applications = new ArrayList<>();
+
+    protected User() {} // JPA Contructor
 
     public User(String name, String email, String password) {
         this.name = name;
@@ -45,5 +70,9 @@ public class User {
 
     public List<Application> getApplications() {
         return applications;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
