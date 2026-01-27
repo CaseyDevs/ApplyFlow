f4ff78ce2b9ae5733d41eea15b46d6f296e56990
CaseyDevs <CaseyDevs5@gmail.com>
2026-01-27 19:47:47 +0000

    chore: add jpa + h2 dependencies

--- src/test/java/com/casey/applyflow/DomainTests.java
+++ src/test/java/com/casey/applyflow/DomainTests.java
@@ -4,7 +4,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.sql.Date;
+
 import com.casey.applyflow.domain.Application;
+import com.casey.applyflow.domain.Company;
+import com.casey.applyflow.domain.Contact;
+import com.casey.applyflow.domain.Interview;
 import com.casey.applyflow.domain.User;
 import com.casey.applyflow.domain.enums.Status;
 
@@ -13,7 +18,11 @@ import com.casey.applyflow.domain.enums.Status;
 public class DomainTests {
     @Test
     void userApplicationsAreIsolated() {
-        Application application = new Application("Backend Developer", "www.fakeurl.com", Status.APPLIED);
+        Company company = new Company("Google", "London", 5.0);
+        Contact contact = new Contact("John", "example@company.com", "+44 00000000000");
+        Interview interview = new Interview(new Date(11000), "Se", contact);
+
+        Application application = new Application("Backend Developer", "www.fakeurl.com", Status.APPLIED, company, interview);
         User user = new User("John Doe", "johndoe@example.com", "password");
         User user2 = new User("Doe John", "doejohn@example.com", "password");
         
