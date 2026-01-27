0e5a0ec1658e46a2c447a422164f7f9946e3f679
CaseyDevs <CaseyDevs5@gmail.com>
2026-01-27 21:20:56 +0000

    feat: add jpa persistence to contact domain

--- src/main/java/com/casey/applyflow/domain/Contact.java
+++ src/main/java/com/casey/applyflow/domain/Contact.java
@@ -1,9 +1,37 @@
 package com.casey.applyflow.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "contact")
 public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column()
     private String email;
+
+    @Column()
     private String phoneNumber;
+
+    @OneToMany(mappedBy = "interviewer")
+    private List<Interview> interviews = new ArrayList<>();
+
+    protected Contact() {} // JPA Constructor
 
     public Contact(String name, String email, String phoneNumber) {
         this.name = name;
@@ -33,5 +61,13 @@ public class Contact {
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
+    }
+
+    public List<Interview> getInterviews() {
+        return interviews;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
