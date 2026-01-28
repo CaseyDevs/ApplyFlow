b073227c04dea839a9bb1cccb14dab17f5860236
CaseyDevs <CaseyDevs5@gmail.com>
2026-01-28 23:30:32 +0000

    chore: return company & interview entity on application response

--- src/main/java/com/casey/applyflow/dto/ApplicationResponseDto.java
+++ src/main/java/com/casey/applyflow/dto/ApplicationResponseDto.java
@@ -1,10 +1,13 @@
 package com.casey.applyflow.dto;
 
+import com.casey.applyflow.domain.Company;
+import com.casey.applyflow.domain.Interview;
 import com.casey.applyflow.domain.enums.Status;
 
 public record ApplicationResponseDto(
     String title,
     String url,
-    Status status
-    // TODO: ADD NOTES, APPLICANTS (TRACK WITH FRIENDS)
+    Status status,
+    Company company,
+    Interview interview
 ) {}
