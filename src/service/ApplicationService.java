16d5c471b07617e98ad4184e1d55361d8235225d
CaseyDevs <CaseyDevs5@gmail.com>
2026-01-28 19:25:57 +0000

    chore: create application request dto

---
+++ src/service/ApplicationService.java
@@ -0,0 +1,15 @@
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApplicationService {
+    
+    // Get Application (Get)
+    
+    // Get Application by...
+
+    // Create Application (Post)
+
+    // Update Applicaiton (Put)
+
+    // Update Apllication (Patch)
+}
