30ae83c23b9149b4746cc9524718fb2b2ebef3d1
CaseyDevs <CaseyDevs5@gmail.com>
2026-02-04 23:07:34 +0000

    feat: implement contact creation business logic

---
+++ src/main/java/com/casey/applyflow/dto/ContactRequestDto.java
@@ -0,0 +1,15 @@
+package com.casey.applyflow.dto;
+
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotBlank;
+
+public record ContactRequestDto(
+    @NotBlank(message = "Contact must have a name.")
+    String name,
+
+    @Nullable String email,
+    @Nullable String phoneNumber,
+
+    @NotBlank(message = "Contact must have a company relationship")
+    Long companyId
+) {}
--- src/main/java/com/casey/applyflow/service/ContactService.java
+++ src/main/java/com/casey/applyflow/service/ContactService.java
@@ -1,24 +1,27 @@
 package com.casey.applyflow.service;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.casey.applyflow.domain.Company;
-import com.casey.applyflow.dto.CompanyResponseDto;
+import com.casey.applyflow.domain.Contact;
+import com.casey.applyflow.dto.ContactRequestDto;
 import com.casey.applyflow.dto.ContactResponseDto;
 import com.casey.applyflow.exception.CompanyNotFoundException;
 import com.casey.applyflow.repository.CompanyRepository;
+import com.casey.applyflow.repository.ContactRepository;
 
 @Service
 public class ContactService {
     private final CompanyRepository companyRepository;
+    private final ContactRepository contactRepository;
 
-    ContactService(CompanyRepository companyRepository) {
+    ContactService(CompanyRepository companyRepository, ContactRepository contactRepository) {
         this.companyRepository = companyRepository;
+        this.contactRepository = contactRepository;
     }
     
     @Transactional(readOnly = true)
@@ -33,5 +36,27 @@ public class ContactService {
                 contact.getEmail(),
                 contact.getPhoneNumber()
             )).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public ContactResponseDto createContact(ContactRequestDto request) {
+        Company company = companyRepository.findById(request.companyId())
+            .orElseThrow(() -> new CompanyNotFoundException("Company does not exist."));
+        
+        Contact contact = new Contact(
+            request.name(), request.email(), request.phoneNumber(), company
+        );
+
+        Contact savedContact = contactRepository.save(contact);
+        company.addInterviewer(savedContact);
+
+        
+
+        return new ContactResponseDto(
+            savedContact.getId(),
+            savedContact.getName(),
+            savedContact.getEmail(),
+            savedContact.getPhoneNumber()
+        );
     }
 }
