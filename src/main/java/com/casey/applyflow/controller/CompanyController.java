1c281597094764fb3ce83826d0572973d44109e9
CaseyDevs <CaseyDevs5@gmail.com>
2026-02-04 21:55:14 +0000

    feat: update company endpoint

--- src/main/java/com/casey/applyflow/controller/CompanyController.java
+++ src/main/java/com/casey/applyflow/controller/CompanyController.java
@@ -15,6 +15,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 
@@ -42,6 +45,16 @@ public class CompanyController {
     ) {
         CompanyResponseDto response = companyService.createCompany(request);
 
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/companies/{id}")
+    public ResponseEntity<CompanyResponseDto> updateCompany(
+        @PathVariable Long id, 
+        @RequestBody CompanyRequestDto request
+    ) {
+        CompanyResponseDto response = companyService.updateCompany(id, request);
+        
         return ResponseEntity.ok(response);
     }
     
