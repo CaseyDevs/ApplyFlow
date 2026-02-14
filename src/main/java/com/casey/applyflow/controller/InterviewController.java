1809eb13138957f9627e45fec335fa5dfb0694e2
CaseyDevs <CaseyDevs5@gmail.com>
2026-02-14 23:53:11 +0000

    feat: interview post mapping

--- src/main/java/com/casey/applyflow/controller/InterviewController.java
+++ src/main/java/com/casey/applyflow/controller/InterviewController.java
@@ -1,5 +1,6 @@
 package com.casey.applyflow.controller;
 
+import com.casey.applyflow.dto.InterviewRequestDto;
 import com.casey.applyflow.dto.InterviewResponseDto;
 import com.casey.applyflow.service.InterviewService;
 
@@ -10,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -29,5 +33,16 @@ public class InterviewController {
         
         return ResponseEntity.ok(response);
     }
+
+    @PostMapping("applications/{id}/interviews")
+    public ResponseEntity<InterviewResponseDto> createInterview(
+        @Valid @PathVariable Long applicationId,
+        @RequestBody InterviewRequestDto request
+    ) {
+        InterviewResponseDto response = interviewService.createInterview(applicationId, request);
+        
+        return ResponseEntity.ok(response);
+    }
+    
     
 }
