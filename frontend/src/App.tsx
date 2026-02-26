b593dc3989384287cef8b23be2fe414e9855ea67
CaseyDevs <CaseyDevs5@gmail.com>
2026-02-26 23:07:17 +0000

    chore: add nav link component, update nav

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar/Navbar';
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import ApplicationsPage from './pages/ApplicationsPage';
+import { useAuth } from './context/AuthContext';
 
 function App() {
+  const { user } = useAuth();
+
   return (
     <>
       <Navbar />
@@ -14,6 +17,12 @@ function App() {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/applications" element={<ApplicationsPage />} />
       </Routes>
+
+      {user 
+        ? <p>Welcome {user.email}</p> 
+        : <p>Please log in...</p>
+      }
+
     </>
   );
 }
---
+++ frontend/src/components/navbar/Navbar.tsx
@@ -0,0 +1,42 @@
+import { useAuth } from "../../context/AuthContext";
+import { logoutUser } from "../../api/auth/logout";
+import "./Navbar.css";
+import { Link, useNavigate } from 'react-router-dom';
+import NavLink from "../NavLink";
+
+export default function Navbar() {
+    const navigate = useNavigate();
+    const { isLoggedIn, refreshUser } = useAuth();
+
+    const handleLogout = async () => {
+        try {
+            await logoutUser();
+            await refreshUser();
+            navigate("/");
+        } catch (err) {
+            console.error("Logout failed:", err);
+        }
+    };
+
+    return (
+        <nav className="navbar">
+            <div className="navbar-brand">
+                <h1><Link to="/" id="nav-logo">ApplyFlow</Link></h1>
+            </div>
+            <ul className="navbar-menu">
+                {isLoggedIn ? (
+                    <>
+                        <NavLink path="/applications">Applications</NavLink>
+                        <NavLink path="/">Job Boards</NavLink>
+                        <NavLink path="/">About</NavLink>
+                        <li><button onClick={handleLogout} className="navbar-button">Logout</button></li>
+                    </>
+                ) : (
+                    <>
+                        <NavLink path="/login">Login</NavLink>
+                    </>
+                )}
+            </ul>
+        </nav>
+    )
+}
