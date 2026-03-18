52931f3c7527f0c6fbbd5477e4cd275a672348ad
CaseyDevs <CaseyDevs5@gmail.com>
2026-03-18 14:28:29 +0000

    chore: update job board application updates to be restful

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -19,7 +19,7 @@ function App() {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/applications" element={<ApplicationsPage />} />
         <Route path="/create-application" element={<CreateApplicationPage />} />
-        <Route path="/applications/:applicationId" element={<UpdateApplicationPage />} />
+        <Route path="/job-board/:jobBoardId/applications/:applicationId" element={<UpdateApplicationPage />} />
         <Route path="/job-boards" element={<JobBoardPage />} />
         <Route path="/job-board/:jobBoardId" element={<JobBoardDetailsPage />} />
         <Route path="/create-job-board" element={<CreateJobBoardPage />} />
