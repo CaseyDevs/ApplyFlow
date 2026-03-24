b45b34d9ad42313187000bb5e64301a7eadef631
CaseyDevs <CaseyDevs5@gmail.com>
2026-03-24 23:25:47 +0000

    refactor: replace useEffect with useQuery in JobBoardPage.tsx

--- frontend/src/pages/JobBoardPage.tsx
+++ frontend/src/pages/JobBoardPage.tsx
@@ -1,40 +1,31 @@
-import { useEffect, useState } from "react"
 import { getJobBoards } from "../api/jobBoardApi";
 import type { JobBoardResponse } from "../types/JobBoard";
 import { useNavigate } from "react-router-dom";
-
+import { useQuery } from "@tanstack/react-query";
 
 export default function JobBoardPage() {
     const navigate = useNavigate();
-    const [jobBoards, setJobBoards] = useState<JobBoardResponse[]>([]);
-    const [loading, setLoading] = useState<boolean>(false);
-    const [errors, setErrors] = useState<string[]>([]);
 
-    // Fetch all job boards to display titles    
-    useEffect(() => {
-        setLoading(true);
-        setErrors([]);
+    // fetch job board data
+    const { data, isPending, error } = useQuery({
+        queryKey: ["job-boards"],
+        queryFn: getJobBoards
+    });
 
-        getJobBoards()
-            .then((jobBoardPage) => {
-                setJobBoards(jobBoardPage.content ?? []);
-            })
-            .catch((err: any) => setErrors([err?.message ?? "Failed to fetch job boards"]))
-            .finally(() => setLoading(false));
-    }, [])
+    const jobBoards = data?.content ?? [];
     
     return (
         <>
-            {loading && <p>Loading...</p>}
-            {errors.length > 0 && errors.map((err, index) => <p key={index}>{err}</p>)}
+            {isPending && <p>Loading...</p>}
+            {error ?? <p>{error}</p>}
 
             <h1>Your Job Boards</h1>
             {jobBoards.length > 0
-                ? jobBoards.map((jb) => 
+                ? jobBoards.map((jb: JobBoardResponse) => 
                     <div key={jb.id}>
                         <button onClick={() => navigate(`/job-boards/${jb.id}`)}>{jb.title}</button>
                     </div>)
-                : !loading && <p>You are not assigned to any job boards!</p>}
+                : !isPending && <p>You are not assigned to any job boards!</p>}
 
             <button onClick={() => navigate("/job-boards/create")}>Create a Job Board</button> 
         </>
