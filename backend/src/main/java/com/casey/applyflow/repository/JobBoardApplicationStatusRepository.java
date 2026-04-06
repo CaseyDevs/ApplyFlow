a18b464f636c5788ea5ebae47ce5dd26362e1fe0
CaseyDevs <CaseyDevs5@gmail.com>
2026-04-06 23:45:33 +0100

    feat: add findAllByJobBoardApplicatio repo method

--- backend/src/main/java/com/casey/applyflow/repository/JobBoardApplicationStatusRepository.java
+++ backend/src/main/java/com/casey/applyflow/repository/JobBoardApplicationStatusRepository.java
@@ -1,5 +1,6 @@
 package com.casey.applyflow.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,6 +8,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.casey.applyflow.model.JobBoardApplicationStatus;
 
 public interface JobBoardApplicationStatusRepository extends JpaRepository<JobBoardApplicationStatus, Long> {
+    Optional<List<JobBoardApplicationStatus>> findAllByJobBoardIdAndApplicationId(
+        Long jobBoardId,
+        Long applicationId
+    );
+
     Optional<JobBoardApplicationStatus> findByJobBoardIdAndApplicationIdAndUserId(
         Long jobBoardId,
         Long applicationId,
